e1238450f030c9b94b690a130ce42de07acadec9
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-11-29 08:37:55 -0500

    for loop to loop through multiple markers, moved javascript code from this into app.js as well

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -14,42 +14,15 @@
 
     <ul>
     @foreach ($workspaces as $workspace)
-    	<li>{{ $workspace->name }}</li>
+    	<li>{{ $workspace->name }}, {{ $workspace->address }}</li>
     @endforeach
   	</ul>
   </div>
 
   <div class="mapContainer">
     <div id="map"></div>
+    <!-- JS code for this is in resources/js/app.js -->
   </div>
-
-  <script>
-
-    function initMap() {
-      var searchedArea = {lat: 38.0406, lng: -84.5037};
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: searchedArea,
-        zoom: 14
-      });
-
-      // add a marker
-      var marker = new google.maps.Marker({
-        position: {lat: 38.04216,lng: -84.4925379999999},
-        map: map
-      });
-
-      // tooltip for marker
-      var infoWindow = new google.maps.InfoWindow({
-        content: '<p><strong>Awesome Inc</strong></p>'
-      });
-
-      // event listener for tooltip to show up on click
-      marker.addListener('click', function() {
-        infoWindow.open(map, marker);
-      });
-    }
-
-  </script>
 
 </div>
 @endsection
