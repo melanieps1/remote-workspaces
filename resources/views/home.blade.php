61d2d5d2d5685b6b5d3b86eb0351af20c2159e00
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-11-29 09:49:30 -0500

    fixed bug where home.blade was slightly wider than screen size

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,18 +10,20 @@
 
 	<div class="floatingForm">
 		<h2>Find workspaces in your city</h2>
-            <form method="POST" action="/results">
-                <div class="searchDiv">
-                    <div class="orangeAccent"></div>
-                    <input class="searchBarHome" type="text" placeholder="Enter a location (city or zip code)">
-                </div>
-            	<input name="_method" type="hidden" value="POST">
-            	{{ csrf_field() }}
+            <div>
+                <form method="POST" action="/results">
+                    <div class="searchDiv">
+                        <div class="orangeAccent"></div>
+                        <input class="searchBarHome" type="text" placeholder="Enter a location (city or zip code)">
+                    </div>
+                	<input name="_method" type="hidden" value="POST">
+                	{{ csrf_field() }}
     			    <button type="submit" name="button" value="search" class="searchBtnHome">
                         <i class="fa fa-search" aria-hidden="true"></i>
     			    	Search
     			    </button>
-          	</form>
+              	</form>
+            </div>
         <p class="grayText">
             Whether you are in your hometown or a new city, use Remote Workspaces to find a spot that fits your needs so you can confidently work from anywhere.
         </p>
